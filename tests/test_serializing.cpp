b1ed325df93868359e4ef968faf78a0492c2d12f
Kacper Kowalski <kacper.s.kowalski@gmail.com>
2022-03-22 12:30:23 +0100

    Extensively test big endian serializing

--- tests/test_serializing.cpp
+++ tests/test_serializing.cpp
@@ -21,7 +21,7 @@ enum class Reg
     field8
 };
 
-TEST_CASE("One byte-long bitfield group is serialized", "[serializing]")
+TEST_CASE("One-byte-long bitfield group is serialized", "[serializing]")
 {
     Bitfield<uint8_t, ByteOrder::big, Field{Reg::field1, 2}, Field{Reg::field2, 4}, Field{Reg::field3, 2}> bf;
 
@@ -30,4 +30,47 @@ TEST_CASE("One byte-long bitfield group is serialized", "[serializing]")
     bf.at<Reg::field3>() = 0b01;
 
     REQUIRE(bf.serialize() == 0b10011001);
+}
+
+TEST_CASE("Two-byte-long bitfield group is serialized, big endian", "[serializing]")
+{
+    SECTION("Evenly aligned, four bitfields")
+    {
+        Bitfield<uint16_t,
+                 ByteOrder::big,
+                 Field{Reg::field1, 4},
+                 Field{Reg::field2, 4},
+                 Field{Reg::field3, 4},
+                 Field{Reg::field4, 4}>
+            bf;
+
+        bf.at<Reg::field1>() = 0b0110;
+        bf.at<Reg::field2>() = 0b1001;
+        bf.at<Reg::field3>() = 0b1010;
+        bf.at<Reg::field4>() = 0b0101;
+
+        REQUIRE(bf.serialize() == 0b0110100110100101);
+    }
+
+    SECTION("Two small bitfields, and one big")
+    {
+        Bitfield<uint16_t, ByteOrder::big, Field{Reg::field1, 1}, Field{Reg::field2, 1}, Field{Reg::field3, 14}> bf;
+
+        bf.at<Reg::field1>() = 0b1;
+        bf.at<Reg::field2>() = 0b0;
+        bf.at<Reg::field3>() = 0b00001111001100;
+
+        REQUIRE(bf.serialize() == 0b1000001111001100);
+    }
+}
+
+TEST_CASE("Four-byte-long bitfield group is serialized, big endian", "[serializing]")
+{
+    Bitfield<uint32_t, ByteOrder::big, Field{Reg::field1, 7}, Field{Reg::field2, 21}, Field{Reg::field3, 4}> bf;
+
+    bf.at<Reg::field1>() = 0b1010101;
+    bf.at<Reg::field2>() = 0b000011111111111110000;
+    bf.at<Reg::field3>() = 0b0110;
+
+    REQUIRE(bf.serialize() == 0b10101010000111111111111100000110);
 }
