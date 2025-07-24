43e4ab99931d09b2c3a023091530cdc6da362f8c
kevin.walker <kevin.walker@plexus.com>
2025-07-24 19:41:27 +0000

    feat: implement clang-format and format all source files

--- tests/test_serializing.cpp
+++ tests/test_serializing.cpp
@@ -5,58 +5,58 @@
  */
 #include <catch2/catch_test_macros.hpp>
 
-#include "jungles/bitfields.hpp"
-
 #include "helpers.hpp"
+#include "jungles/bitfields.hpp"
 
 using namespace jungles;
 
-TEST_CASE("One-byte-long bitfield group is serialized", "[serializing]")
-{
-    Bitfields<uint8_t, Field<Reg::field1, 2>, Field<Reg::field2, 4>, Field<Reg::field3, 2>> bf;
+TEST_CASE("One-byte-long bitfield group is serialized", "[serializing]") {
+  Bitfields<uint8_t, Field<Reg::field1, 2>, Field<Reg::field2, 4>,
+            Field<Reg::field3, 2>>
+      bf;
 
-    bf.at<Reg::field1>() = 0b10;
-    bf.at<Reg::field2>() = 0b0110;
-    bf.at<Reg::field3>() = 0b01;
+  bf.at<Reg::field1>() = 0b10;
+  bf.at<Reg::field2>() = 0b0110;
+  bf.at<Reg::field3>() = 0b01;
 
-    REQUIRE(bf.serialize() == 0b10011001);
+  REQUIRE(bf.serialize() == 0b10011001);
 }
 
-TEST_CASE("Two-byte-long bitfield group is serialized", "[serializing]")
-{
-    SECTION("Evenly aligned, four bitfields")
-    {
-        Bitfields<uint16_t, Field<Reg::field1, 4>, Field<Reg::field2, 4>, Field<Reg::field3, 4>, Field<Reg::field4, 4>>
-            bf;
+TEST_CASE("Two-byte-long bitfield group is serialized", "[serializing]") {
+  SECTION("Evenly aligned, four bitfields") {
+    Bitfields<uint16_t, Field<Reg::field1, 4>, Field<Reg::field2, 4>,
+              Field<Reg::field3, 4>, Field<Reg::field4, 4>>
+        bf;
 
-        bf.at<Reg::field1>() = 0b0110;
-        bf.at<Reg::field2>() = 0b1001;
-        bf.at<Reg::field3>() = 0b1010;
-        bf.at<Reg::field4>() = 0b0101;
+    bf.at<Reg::field1>() = 0b0110;
+    bf.at<Reg::field2>() = 0b1001;
+    bf.at<Reg::field3>() = 0b1010;
+    bf.at<Reg::field4>() = 0b0101;
 
-        REQUIRE(bf.serialize() == 0b0110100110100101);
-    }
+    REQUIRE(bf.serialize() == 0b0110100110100101);
+  }
 
-    SECTION("Two small bitfields, and one big")
-    {
-        Bitfields<uint16_t, Field<Reg::field1, 1>, Field<Reg::field2, 1>, Field<Reg::field3, 14>> bf;
+  SECTION("Two small bitfields, and one big") {
+    Bitfields<uint16_t, Field<Reg::field1, 1>, Field<Reg::field2, 1>,
+              Field<Reg::field3, 14>>
+        bf;
 
-        bf.at<Reg::field1>() = 0b1;
-        bf.at<Reg::field2>() = 0b0;
-        bf.at<Reg::field3>() = 0b00001111001100;
+    bf.at<Reg::field1>() = 0b1;
+    bf.at<Reg::field2>() = 0b0;
+    bf.at<Reg::field3>() = 0b00001111001100;
 
-        REQUIRE(bf.serialize() == 0b1000001111001100);
-    }
+    REQUIRE(bf.serialize() == 0b1000001111001100);
+  }
 }
 
-TEST_CASE("Four-byte-long bitfield group is serialized", "[serializing]")
-{
-    Bitfields<uint32_t, Field<Reg::field1, 7>, Field<Reg::field2, 21>, Field<Reg::field3, 4>> bf;
+TEST_CASE("Four-byte-long bitfield group is serialized", "[serializing]") {
+  Bitfields<uint32_t, Field<Reg::field1, 7>, Field<Reg::field2, 21>,
+            Field<Reg::field3, 4>>
+      bf;
 
-    bf.at<Reg::field1>() = 0b1010101;
-    bf.at<Reg::field2>() = 0b000011111111111110000;
-    bf.at<Reg::field3>() = 0b0110;
+  bf.at<Reg::field1>() = 0b1010101;
+  bf.at<Reg::field2>() = 0b000011111111111110000;
+  bf.at<Reg::field3>() = 0b0110;
 
-    REQUIRE(bf.serialize() == 0b10101010000111111111111100000110);
+  REQUIRE(bf.serialize() == 0b10101010000111111111111100000110);
 }
-
--- tests/test_various_id_types.cpp
+++ tests/test_various_id_types.cpp
@@ -1,43 +1,34 @@
 /**
  * @file	test_various_id_types.cpp
- * @brief	Creates bitfields with various ID types, to check whether it compiles.
+ * @brief	Creates bitfields with various ID types, to check whether it
+ * compiles.
  * @author	Kacper Kowalski - kacper.s.kowalski@gmail.com
  */
 #include "jungles/bitfields.hpp"
 
 using namespace jungles;
 
-int main(void)
-{
-    auto compile_enum_as_id{[]() {
-        enum SomeEnum
-        {
-            EnumVal1,
-            EnumVal2,
-            EnumVal3
-        };
+int main(void) {
+  auto compile_enum_as_id{[]() {
+    enum SomeEnum { EnumVal1, EnumVal2, EnumVal3 };
 
-        Bitfields<uint8_t, Field<EnumVal1, 2>, Field<EnumVal2, 3>, Field<EnumVal3, 3>>{};
-    }};
+    Bitfields<uint8_t, Field<EnumVal1, 2>, Field<EnumVal2, 3>,
+              Field<EnumVal3, 3>>{};
+  }};
 
-    auto compile_int_as_id{[]() {
-        Bitfields<uint8_t, Field<10, 2>, Field<20, 3>, Field<30, 3>>{};
-    }};
+  auto compile_int_as_id{
+      []() { Bitfields<uint8_t, Field<10, 2>, Field<20, 3>, Field<30, 3>>{}; }};
 
-    auto compile_enum_class_as_id{[]() {
-        enum class SomeEnumClass
-        {
-            V1,
-            V2,
-            V3
-        };
+  auto compile_enum_class_as_id{[]() {
+    enum class SomeEnumClass { V1, V2, V3 };
 
-        Bitfields<uint8_t, Field<SomeEnumClass::V1, 2>, Field<SomeEnumClass::V2, 3>, Field<SomeEnumClass::V3, 3>>{};
-    }};
+    Bitfields<uint8_t, Field<SomeEnumClass::V1, 2>, Field<SomeEnumClass::V2, 3>,
+              Field<SomeEnumClass::V3, 3>>{};
+  }};
 
-    compile_enum_as_id();
-    compile_int_as_id();
-    compile_enum_class_as_id();
+  compile_enum_as_id();
+  compile_int_as_id();
+  compile_enum_class_as_id();
 
-    return 0;
+  return 0;
 }
